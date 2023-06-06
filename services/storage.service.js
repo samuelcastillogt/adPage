d29e405844445976005cbeff2ef61c2e61dc91cb
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2023-06-05 20:25:16 -0600

    integracion de UUID

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,8 +1,12 @@
 import { storage } from "./configFirebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-const imagesRef = ref(storage, 'images');
+import { v4 as uuidv4 } from 'uuid';
+
+
 class StorageService{
     uploadFile= async(file)=>{
+        const name = uuidv4();
+        const imagesRef = ref(storage, name);
         const snapshot = await uploadBytes(imagesRef, file)
         const downloadURL = await getDownloadURL(snapshot.ref)
         return downloadURL
